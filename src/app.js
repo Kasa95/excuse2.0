adf5f995151461715b5c3929e216b90b408c8669
Kasa95 <nachocasanova95@gmail.com>
2022-05-09 19:36:30 +0000

    primera versión

--- src/app.js
+++ src/app.js
@@ -6,6 +6,54 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = function() {
+  let who = [
+    "My dog",
+    "My grandmother",
+    "A policeman",
+    "Michael Jackson",
+    "The President of the United States"
+  ];
+  let verb = ["ate", "destroyed", "peed on", "swallowed"];
+  let what = [
+    "my homework",
+    "my coding assignment",
+    "my car keys",
+    "my driving license"
+  ];
+  let when = [
+    "this morning",
+    "three days ago",
+    "just before I got here",
+    "right before I came into the room"
+  ];
+  let consequence = [
+    "and now I'm slightly depressed",
+    "and now I want to cry",
+    "and now I don't know what to do",
+    "so I'll just go",
+    "and I'm not feeling that good anymore"
+  ];
+
+  var whoIndex = Math.floor(Math.random() * who.length);
+  let verbIndex = Math.floor(Math.random() * verb.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  let consequenceIndex = Math.floor(Math.random() * consequence.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    verb[verbIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex] +
+    " " +
+    consequence[consequenceIndex] +
+    "."
+  );
 };
